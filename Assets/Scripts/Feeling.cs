c12bb33b321cd961887d53090a2b1b07dc60423b
Derek Andes <derek.m.andes@gmail.com>
2016-02-29 23:13:48 -0600

    Add simple kid feedback

--- Assets/Scripts/Feeling.cs
+++ Assets/Scripts/Feeling.cs
@@ -3,5 +3,32 @@ using System.Collections;
 
 public class Feeling : MonoBehaviour
 {
-    public bool content = false;
-}   
+    private ObjectManager objectManager;
+    private SpriteRenderer body;
+
+    public bool happy = false;
+    public bool likeRain = false;
+
+    void Start()
+    {
+        objectManager = gameObject.GetComponent<ObjectManager>();
+        body = gameObject.GetComponentInChildren<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        if (objectManager.HoldingObject())
+        {
+            happy = !likeRain;
+            
+        }
+        else
+        {
+            happy = likeRain;
+            
+        }
+
+        //Set body to green for happy, pink for unhappy
+        body.color = happy ? new Color32(110, 231, 196, 255) : new Color32(231, 110, 143, 255);
+    }
+}
