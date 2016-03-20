68fdf15a2cc5bfeb7c5f8ba3062483b1795f0d0c
Derek Andes <derek.m.andes@gmail.com>
2016-03-20 06:50:59 -0500

    Add sprite sorting

---
+++ Assets/Scripts/SpriteSorter.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpriteSorter : MonoBehaviour
+{
+    private SpriteRenderer sprite;
+    private Transform parent;
+
+	void Start ()
+	{
+        sprite = GetComponent<SpriteRenderer>();
+        parent = transform.root;
+	}
+
+	void LateUpdate ()
+	{
+        sprite.sortingOrder = (int)Camera.main.WorldToScreenPoint(parent.position).y * -1;
+    }
+}
